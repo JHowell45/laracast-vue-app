f3f18e797e01bd96732e552fadee1c5167a80416
JHowell45 <jhowell45@googlemail.com>
2020-04-02 11:53:43 +0100

    Finished ep 9 on sub components.

--- episode_8/main.js
+++ episode_8/main.js
@@ -1,3 +1,23 @@
+Vue.component("task-list", {
+  template: `
+      <div>
+        <task v-for='task in tasks'>
+          {{ task.desc }}
+        </task>
+      </div>
+  `,
+
+  data() {
+    return {
+      tasks: [
+        { desc: "Go to the store", complete: true },
+        { desc: "Go to the bank", complete: false },
+        { desc: "Go to work", complete: true }
+      ]
+    };
+  }
+});
+
 Vue.component("task", {
   template: "<ul><li><slot></slot></li></ul>"
 });
