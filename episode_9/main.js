cd212fd1603612a74a784acb3c1f3b5521b9835c
JHowell45 <jhowell45@googlemail.com>
2020-04-02 12:37:00 +0100

    working on episode 9.

--- episode_9/main.js
+++ episode_9/main.js
@@ -1,26 +1,24 @@
-Vue.component("task-list", {
+Vue.component("message", {
   template: `
-      <div>
-        <task v-for='task in tasks'>
-          {{ task.desc }}
-        </task>
+    <article class="message">
+      <div class="message-header">
+        <p>Hello World</p>
       </div>
-  `,
-
-  data() {
-    return {
-      tasks: [
-        { desc: "Go to the store", complete: true },
-        { desc: "Go to the bank", complete: false },
-        { desc: "Go to work", complete: true }
-      ]
-    };
-  }
+      <div class="message-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta
+        nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida
+        purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac
+        <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
+        sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi
+        magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales
+        sem.
+      </div>
+    </article>
+  `
 });
 
-Vue.component("task", {
-  template: "<ul><li><slot></slot></li></ul>"
-});
+Vue.component("message", { template: "<p></p>" });
 
 new Vue({
   el: "#root"
