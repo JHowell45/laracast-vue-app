862d50bfc5394702343862d44edee9ed36c1f219
JHowell45 <jhowell45@googlemail.com>
2020-04-03 15:47:28 +0100

    Finished the episode.

--- episode_12/main.js
+++ episode_12/main.js
@@ -1,13 +1,27 @@
 Vue.component("coupon", {
-  template: `<input>`
+  template: `
+  
+    <input placeholder="Enter your coupon code" @blur="onCouponApplied">
+    
+  `,
+
+  methods: {
+    onCouponApplied() {
+      this.$emit("applied");
+    }
+  }
 });
 
 new Vue({
   el: "#root",
 
+  data: {
+    couponApplied: false
+  },
+
   methods: {
     onCouponApplied() {
-      alert("Coupon applied!");
+      this.couponApplied = true;
     }
   }
 });
