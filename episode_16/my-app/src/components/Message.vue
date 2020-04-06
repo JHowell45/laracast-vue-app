e52a0ab94887c843b7a2ef925c0ed982f71d505f
JHowell45 <jhowell45@googlemail.com>
2020-04-06 11:12:48 +0100

    Completed episode 16.

---
+++ episode_16/my-app/src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box is-info">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+<style>
+.box {
+  background: #c5c5c5;
+  padding: 10px;
+  border: 1px solid grey;
+  margin-bottom: 10px;
+}
+</style>
