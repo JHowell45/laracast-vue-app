1931a37bd98d34709e5392523c295122b5c82b7d
JHowell45 <jhowell45@googlemail.com>
2020-04-06 15:39:10 +0100

    Creating base form for lesson.

---
+++ 19-20-21 - Object Oriented Forms/forms-lesson/src/components/Form.vue
@@ -0,0 +1,23 @@
+<template>
+  <form method="POST" action="">
+    <div class="control">
+      <label for="name" class="label">Project Name: </label>
+      <input type="text" id="name" name="name" class="input" />
+    </div>
+
+    <div class="control">
+      <label for="description" class="label">Project Description: </label>
+      <input type="text" id="description" name="description" class="input" />
+    </div>
+
+    <div class="control">
+      <button class="button-is-primary">Create</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
