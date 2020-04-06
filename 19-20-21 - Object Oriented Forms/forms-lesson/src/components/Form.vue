5e6fd60691883551a0c5b277f3b20a65a874bc9f
JHowell45 <jhowell45@googlemail.com>
2020-04-06 16:02:47 +0100

    Updated how the form works.

--- 19-20-21 - Object Oriented Forms/forms-lesson/src/components/Form.vue
+++ 19-20-21 - Object Oriented Forms/forms-lesson/src/components/Form.vue
@@ -1,13 +1,19 @@
 <template>
-  <form method="POST" action="">
+  <form method="POST" action="/projects" @submit="onSubmit">
     <div class="control">
       <label for="name" class="label">Project Name: </label>
-      <input type="text" id="name" name="name" class="input" />
+      <input type="text" id="name" name="name" class="input" v-model="name" />
     </div>
 
     <div class="control">
       <label for="description" class="label">Project Description: </label>
-      <input type="text" id="description" name="description" class="input" />
+      <input
+        type="text"
+        id="description"
+        name="description"
+        class="input"
+        v-model="description"
+      />
     </div>
 
     <div class="control">
@@ -17,7 +23,14 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "",
+      description: "",
+    };
+  },
+};
 </script>
 
 <style scoped></style>
