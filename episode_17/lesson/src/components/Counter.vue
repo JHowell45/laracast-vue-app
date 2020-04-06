eeb9c4e0395aa81114f663e0f7697e5321730f20
JHowell45 <jhowell45@googlemail.com>
2020-04-06 12:03:23 +0100

    Finished episode 17.

---
+++ episode_17/lesson/src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Count: {{ count }}</h2>
+
+    <p>
+      <button @click="count += 1">Increment Count</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<style></style>
