0e05e3007e4459fabd130e004f561e2a6671a588
JHowell45 <jhowell45@googlemail.com>
2020-04-07 13:12:19 +0100

    Finished ep 19.

--- 19-20-21 - Object Oriented Forms/forms-lesson/src/components/Form.vue
+++ 19-20-21 - Object Oriented Forms/forms-lesson/src/components/Form.vue
@@ -1,5 +1,10 @@
 <template>
-  <form method="POST" action="/projects" @submit.prevent="onSubmit">
+  <form
+    method="POST"
+    action="/projects"
+    @submit.prevent="onSubmit"
+    @keydown="errors.clear($event.target.name)"
+  >
     <div class="control">
       <label for="name" class="label">Project Name: </label>
       <input
@@ -11,7 +16,11 @@
         required
       />
 
-      <span class="help is-danger" v-text="errors.get('name')"></span>
+      <span
+        class="help is-danger"
+        v-if="errors.has('name')"
+        v-text="errors.get('name')"
+      ></span>
     </div>
 
     <div class="control">
@@ -19,28 +28,63 @@
       <input
         type="text"
         id="description"
-        name="description"
+        name=""
         class="input"
         v-model="description"
         required
       />
     </div>
 
+    <span
+      class="help is-danger"
+      v-if="errors.has('description')"
+      v-text="errors.get('description')"
+    ></span>
+
     <div class="control">
-      <button class="button is-primary">Create</button>
+      <button class="button is-primary" :disabled="errors.any()">Create</button>
     </div>
   </form>
 </template>
 
 <script>
 import axios from "axios";
+// import "core-js/es6/object";
+
+class Errors {
+  constructor() {
+    this.errors = {};
+  }
+
+  get(field) {
+    if (this.errors[field]) {
+      return this.errors[field][0];
+    }
+  }
+
+  record(errors) {
+    this.errors = errors;
+  }
+
+  clear(field) {
+    delete this.errors[field];
+  }
+
+  has(field) {
+    Object.prototype.hasOwnProperty.call(this.errors, field);
+  }
+
+  any() {
+    return Object.keys(this.errors).length > 0;
+  }
+}
 
 export default {
   data() {
     return {
       name: "",
       description: "",
-      errors: {},
+      errors: new Errors()
     };
   },
 
@@ -50,13 +94,16 @@ export default {
 
       axios
         .post("/projects", this.$data)
-        .then((response) => {
-          alert("success!");
-          return response;
-        })
-        .catch((error) => (this.errors = error.response.data));
+        .then(this.onSuccess)
+        .catch(error => this.errors.record(error.response.data));
     },
-  },
+    onSuccess(response) {
+      alert("success!");
+      this.name = "";
+      this.description = "";
+      return response;
+    }
+  }
 };
 </script>
 
